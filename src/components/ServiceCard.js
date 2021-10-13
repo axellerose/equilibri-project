cf485cfa4164d3642378db87085761e31cbad9d6
alex <iamalex.dev@gmail.com>
2021-10-13 12:37:36 +0200

    services mobile

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { between } from 'polished'
 import styled from 'styled-components'
+import { down } from '../mixins'
 
 
 const Card = styled.div`
@@ -22,15 +23,82 @@ width: ${between('140px', '450px', '600px', '2560px')};
     color: #808080;
     font-size: ${between('14px', '20px', '600px', '2560px')};
   }
+
+  ${down.md`
+    width: 100%;
+    display: flex;
+    
+    padding-bottom: 25px; 
+
+    & img {
+      height: 150px;
+      width: 150px;
+    }
+
+    & .service__text {
+      padding-left: 25px;
+    }
+
+    &.even {
+      display: flex;
+      flex-direction: row-reverse;
+
+      & .service__text {
+        padding-left: 0;
+        padding-right: 25px;
+      }
+    }
+  `}
+  ${down.sm`
+    width: 100%;
+    display: flex;
+    flex-diretion: column;
+    justify-content: center;
+    
+    padding-bottom: 25px; 
+
+    & img {
+      height: 150px;
+      width: 150px;
+    }
+
+    & .service__image, service__text {
+      padding-top: 10px;
+      padding-left: 0;
+      padding-right: 0;
+      margin: 0 auto;
+    }
+
+    &.even {
+      flex-direction: column;
+      & .service__text {
+        padding-left: 0;
+        padding-right: 0;
+      }
+    }
+    &.odd {
+      flex-direction: column;
+      & .service__text {
+        padding-left: 0;
+        padding-right: 0;
+      }
+    }
+  `}
 `
 
-const ServiceCard = ({ image, text, title }) => {
+const ServiceCard = ({ image, text, title, position }) => {
 
   return (
-    <Card>
-      <img src={image} alt="service" />
-      <h3>{title} </h3>
-      <p>{text}</p>
+    <Card className={position}>
+      <div className="service__image">
+        <img src={image} alt="service" />
+      </div>
+
+      <div className="service__text">
+        <h3>{title} </h3>
+        <p>{text}</p>
+      </div>
+
     </Card>
   )
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -7,12 +7,17 @@ import image1 from '../assets/signature.jpg'
 import image2 from '../assets/gabriel.jpg'
 import image3 from '../assets/office.png'
 import image4 from '../assets/columns.jpg'
+import { down } from '../mixins'
 
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 0 75px 100px 75px;
+
+  ${down.sm`
+    padding: 0 20px 50px 20px;
+  `}
 `
 
 const Title = styled.h1`
@@ -23,6 +28,7 @@ const Title = styled.h1`
   font-size: ${between('42px', '96px', '600px', '2560px')};
   font-weight: 400;
   color: #7BA0C4;
+
 `
 
 const CardsList = styled.div`
@@ -30,6 +36,11 @@ width: 100%;
 display: flex;
 justify-content: space-between;
 width: 100%;
+
+${down.lg`
+flex-wrap: wrap;
+`}
+
 `
 
 const Services = () => {
@@ -39,10 +50,10 @@ const Services = () => {
         Serveis
       </Title>
       <CardsList>
-        <ServiceCard image={image1} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
-        <ServiceCard image={image2} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
-        <ServiceCard image={image3} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
-        <ServiceCard image={image4} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
+        <ServiceCard position="odd" image={image1} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
+        <ServiceCard position="even" image={image2} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
+        <ServiceCard position="odd" image={image3} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
+        <ServiceCard position="even" image={image4} title="LOREM IPSUM" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl." />
       </CardsList>
     </Wrapper>
   )
